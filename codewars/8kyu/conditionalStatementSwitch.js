d7f93ef6a2b7fbe548889d88b7ccdbc7ae233bf6
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2022-01-17 11:59:55 +0000

    add conditional statement switch

---
+++ codewars/8kyu/conditionalStatementSwitch.js
@@ -0,0 +1,48 @@
+/*
+witch is the keyword and n is the variable to switch. case 1 means when n===1. Following the ":" you can add your code for what to do in that case. The keyword break is used as termination - if you forget break, the code will continue running through the other case statements and default until a break appears. If no case statements match, default is entered.
+
+In some instances, the switch statement is clearer than the if..else statement.
+*/
+
+function howManydays(month) {
+    let days;
+    switch (month) {
+        case 1:
+            days = 31;
+            break;
+        case 2:
+            days = 28;
+            break;
+        case 3:
+            days = 31;
+            break;
+        case 4:
+            days = 30;
+            break;
+        case 5:
+            days = 31;
+            break;
+        case 6:
+            days = 30;
+            break;
+        case 7:
+            days = 31;
+            break;
+        case 8:
+            days = 31;
+            break;
+        case 9:
+            days = 30;
+            break;
+        case 10:
+            days = 31;
+            break;
+        case 11:
+            days = 30;
+            break;
+        case 12:
+            days = 31;
+            break;
+    }
+    return days;
+}
