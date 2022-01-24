ed2e5c3eb7e2fd9be33d111e82bcd1753ce579c9
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2022-01-24 14:35:16 +0000

    add minimize sum of array

---
+++ codewars/7kyu/minimizeSumOfArray1.js
@@ -0,0 +1,23 @@
+/*
+Task
+Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
+
+Notes
+Array/list will contain positives only .
+Array/list will always have even size
+Input >> Output Examples
+minSum({5,4,2,3}) ==> return (22) 
+Explanation:
+The minimum sum obtained from summing each two integers product ,  5*2 + 3*4 = 22
+*/
+
+function minSum(arr) {
+    arr.sort((a,b)=>a-b)
+    let output = 0;
+    while (arr.length > 0){
+      output += arr[0]*arr[arr.length-1];
+      arr.pop();
+      arr.shift();
+    };
+    return output;
+}
