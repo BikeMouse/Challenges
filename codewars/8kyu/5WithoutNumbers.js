9cdf4723900cb72f6874124df1f846a25f4a479d
BikeMouse <99062030+BikeMouse@users.noreply.github.com>
2022-02-10 12:58:38 +0000

    add 5 without numbers

---
+++ codewars/8kyu/5WithoutNumbers.js
@@ -0,0 +1,12 @@
+/*
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+
+function unusualFive() {
+    let a = ['a','b','c','d','e'];
+    return a.length;
+}
