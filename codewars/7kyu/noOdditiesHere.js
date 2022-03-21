6730042138e306ce646c786f1225c2b73db3c6f1
BikeMouse <99062030+BikeMouse@users.noreply.github.com>
2022-03-21 15:23:21 +0000

    add no oddities here

---
+++ codewars/7kyu/noOdditiesHere.js
@@ -0,0 +1,17 @@
+/*
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+
+
+*/
+
+function noOdds( values ){
+    let result = []
+    values.forEach((e)=>{
+      if (e % 2 == 0){
+        result.push(e);
+      };
+    });
+    return result;
+}
