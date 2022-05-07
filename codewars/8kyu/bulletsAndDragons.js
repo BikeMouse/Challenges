8ce627fbcac6867f8cc3303645803a5d21c9b888
BikeMouse <99062030+BikeMouse@users.noreply.github.com>
2022-05-07 23:48:12 +0100

    add fundamental challenge

---
+++ codewars/8kyu/bulletsAndDragons.js
@@ -0,0 +1,10 @@
+function hero(bullets, dragons){
+    //Get Coding!
+      //more bullets then dragons
+      // 2 bullets kill 1 dragon
+      if (bullets > dragons && bullets >= (2*dragons)){
+        return true
+        } else {
+          return false
+        }
+    }
