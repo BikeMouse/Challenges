d31f0d8832fb118851f36e34a514817ca70b9800
BikeMouse <99062030+BikeMouse@users.noreply.github.com>
2022-06-17 00:31:36 +0100

    add twice as old

---
+++ codewars/8kyu/twicsAsOld.js
@@ -0,0 +1,12 @@
+/*
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+*/
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let result = (dadYearsOld-(sonYearsOld*2));
+    return (result >= 0 ? result : result*(-1))
+}
