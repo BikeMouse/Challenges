3d00fa344d4fa332974fc7fb234f64462dc917a6
BikeMouse <99062030+BikeMouse@users.noreply.github.com>
2022-06-17 12:36:29 +0100

    add find the remainder

---
+++ codewars/8kyu/findTheRemainder.js
@@ -0,0 +1,13 @@
+/*
+Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+Division by zero should return NaN.
+
+*/
+
+function remainder(n, m){
+    // 2 Integers, positive and negative
+    // Remainder, integer
+    // 13%6 = 1
+    return (n > m ? (n % m) : (m % n));
+}
