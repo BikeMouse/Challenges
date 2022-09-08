131fc359190af8b9e9c7190ecab4ae018b0cf6e1
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2022-09-08 15:06:16 +0100

    add axclamation mark series 6

---
+++ codewars/8kyu/exclamationMarkSeries6.js
@@ -0,0 +1,29 @@
+/*
+Description:
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+*/
+
+function remove(s,n){
+    let result = '';
+    let count = 0;
+    for (let i = 0; i < s.length; i++){
+      if (count < n && s[i] == '!'){
+        count++
+      } else{
+        result += s[i]
+      }
+    } return result
+}
