9bfe8ef902e71357fe872373e9d152ad37b5f7b1
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2022-10-12 12:37:39 +0100

    add backspaces in a string

---
+++ codewars/6kyu/backSpacesInAString.js
@@ -0,0 +1,15 @@
+/*
+Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+Your task is to process a string with "#" symbols.
+
+Examples
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+*/
+
+function cleanString(s) {
+    return s.split('').reduce((acc,c)=> c === '#' ? acc.substr(0,acc.length-1) : acc += c, '')
+}
