bd4ef745f1b464a58dd81287e5d5712151908645
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2022-10-26 21:49:44 +0100

    add get planet name by id

---
+++ codewars/8kyu/getPlanetNameByID.js
@@ -0,0 +1,12 @@
+/*
+The function is not returning the correct values. Can you figure out why?
+
+Example (Input --> Output ):
+
+3 --> "Earth"
+*/
+
+function getPlanetName(id) {
+    let planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+    return planets[id - 1];
+}
