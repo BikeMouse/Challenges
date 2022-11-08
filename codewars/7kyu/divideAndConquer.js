1e766edb4dea10f15bb707f01be8b875919d4dd2
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2022-11-08 15:19:26 +0000

    add divide and conquer

---
+++ codewars/7kyu/divideAndConquer.js
@@ -0,0 +1,20 @@
+/*
+Given a mixed array of number and string representations of integers, add up the non-string integers and subtract this from the total of the string integers.
+
+Return as a number.
+
+
+*/
+
+function divCon(x) {
+    let nums = 0;
+    let stri = 0;
+    for (let i = 0; i < x.length; i++) {
+        if (typeof x[i] === 'number') {
+            nums += x[i];
+        } else {
+            stri += Number(x[i]);
+        };
+    };
+    return nums - stri;
+}
