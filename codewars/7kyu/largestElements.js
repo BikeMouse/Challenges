1ec37725738b15dc66fa2c5787e25fa25badbb72
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2022-11-15 16:57:56 +0000

    add largest elements

---
+++ codewars/7kyu/largestElements.js
@@ -0,0 +1,13 @@
+/*
+Write a program that outputs the top n elements from a list.
+
+Example:
+
+largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+*/
+
+function largest(n, xs) {
+    xs.sort((a, b) => b - a);
+    return xs.slice(0, n).reverse();
+}
