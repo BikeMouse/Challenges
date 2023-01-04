9330748d0861396f3293dd71fed30023af0b62bb
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2023-01-04 22:18:46 +0000

    add working with arrays II

---
+++ codewars/7kyu/workingWithArraysII.js
@@ -0,0 +1,13 @@
+/*
+The function returns an array/list like the one passed to it but with its nth element removed (with 0 <= n <= array/list.length - 1). The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+Good luck!
+
+
+*/
+
+function removeNthElement(arr, n) {
+    var arrCopy = [...arr];
+    arrCopy.splice(n, 1);
+    return arrCopy;
+}
