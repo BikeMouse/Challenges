87c518028d40d2ddb11282e82ad4a49cd27a3ae5
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2023-04-08 16:39:13 +0100

    add mispelled word

---
+++ codewars/7kyu/mispelledWord.js
@@ -0,0 +1,36 @@
+/*
+Create a function mispelled(word1, word2):
+
+mispelled('versed', 'xersed'); // returns true
+mispelled('versed', 'applb'); // returns false
+mispelled('versed', 'v5rsed'); // returns true
+mispelled('1versed', 'versed'); // returns true
+mispelled('versed', 'versed'); // returns true
+It checks if the word2 differs from word1 by at most one character.
+
+This can include an extra char at the end or the beginning of either of words.
+
+In the tests that expect true, the mispelled word will always differ mostly by one character. If the two words are the same, return True.
+*/
+
+const mispelled = function (word1, word2) {
+    let count = 0;
+    if (word1.length == word2.length) {
+        for (let i = 0; i < word1.length; i++) {
+            if (word1[i] !== word2[i]) count++;
+        };
+    } else if (word1.length > word2.length) {
+        if ((word1.slice(1) == word2) || (word1.slice(0, -1) == word2)) {
+            return true;
+        } else {
+            return false;
+        };
+    } else {
+        if ((word2.slice(1) == word1) || (word2.slice(0, -1) == word1)) {
+            return true;
+        } else {
+            return false;
+        };
+    }
+    return (count > 1 ? false : true);
+}
