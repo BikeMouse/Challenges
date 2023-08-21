377264ca5dc3c3ee6ef26514c6d06e24a00e1d13
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2023-08-21 18:07:56 +0100

    add stringy strings

---
+++ codewars/8kyu/stringyStrings.py
@@ -0,0 +1,21 @@
+# write me a function stringy that takes a size and returns 
+# a string of alternating 1s and 0s.
+
+# the string should start with a 1.
+
+# a string with size 6 should return :'101010'.
+
+# with size 4 should return : '1010'.
+
+# with size 12 should return : '101010101010'.
+
+# The size will always be positive and will only use whole numbers.
+
+def stringy(size):
+    output = ''
+    for i in range(size):
+        if i % 2 == 0:
+            output += '1'
+        else:
+            output += '0'
+    return output
