706ffd06dd4dc479f0c1fb2b671bb17223723a5c
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2023-08-30 23:48:38 +0100

    add fixme replace all dots

---
+++ codewars/8kyu/fixmeReplaceAllDots.py
@@ -0,0 +1,18 @@
+# The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+# But it's not working properly.
+
+# Task
+# Fix the bug so we can all go home early.
+
+# Notes
+# String str will never be null.
+
+def replace_dots(s):
+    output = ''
+    for i in range(len(s)):
+        if s[i] == '.':
+            output += '-'
+        else:
+            output += s[i]
+    return output
