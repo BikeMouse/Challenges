21836b69124fbe92340e50af2e31cb43d6eae0d1
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2023-09-20 19:41:23 +0100

    add more than zero

---
+++ codewars/7kyu/moreThanZero.py
@@ -0,0 +1,10 @@
+# Correct this code so that it takes one argument, x, and 
+# returns "x is more than zero" if x is positive (and nonzero), 
+# and otherwise, returns "x is equal to or less than zero." In both 
+# cases, replace x with the actual value of x.
+
+def corrections(x):
+    if x > 0:
+        return "{x} is more than zero.".format(x = x)
+    else:
+        return "{x} is equal to or less than zero.".format(x = x)
